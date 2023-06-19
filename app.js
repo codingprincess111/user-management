4bb5376bd16778a6826f129fbb9cdb0c7f6e6f45
codingprincess111 <100244609+codingprincess111@users.noreply.github.com>
2023-06-19 15:32:14 -0500

    user management complete with repo

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// parsing middleware 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// static files
+app.use(express.static('public'));
+
+// template engine
+app.engine('hbs', exphbs.engine( {extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+
+
+
+
+
+const routes = require('./server/routes/user');
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
